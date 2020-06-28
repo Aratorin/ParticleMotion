5119ce0a6bddfe8397d21b874850315326c4b837
Aratorin <johnjhofmann3@gmail.com>
2020-06-28 16:42:56 -0400

    Changed the color of pixels.
    - Used memset to set all bytes of the buffer to 0. This results in a
      black screen.
    - Used a for loop to set each Uint32 (4 bytes *ie* 1 pixel) in the
      buffer to 0x00FF00FF (green).
    - Set the 4 pixels in the center of the screen to 0xFF0000FF (red).

--- ParticleMotion/ParticleMotion.cpp
+++ ParticleMotion/ParticleMotion.cpp
@@ -37,7 +37,32 @@ int main(int argc, char* argv[]) {
 
 	Uint32* buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
 
-	memset(buffer, 0xFF, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+	memset(buffer, 0x00, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+
+	/*
+		Each row has SCREEN_WIDTH - 1 pixels and there are SCREEN_HEIGHT rows
+		So the x position of a given pixel is between 0 and SCREEN_WIDTH-1(because arrays run 0 to length - 1)
+		The y position of a given pixel is SCREEN_WIDTH * Desired y position because each row contains SCREEN_WIDTH - 1 pixels
+		To set the x,y of the pixel you are editing, you add the x and y coordinates together to get the pixel you are setting
+		A screen with an even number of pixels has no center pixel, so the "center" of the screen is the 4 pixel square:
+		((SCREEN_WIDTH / 2) - 1) + ((SCREEN_HEIGHT / 2) * SCREEN_WIDTH) //top left
+		(SCREEN_WIDTH / 2) + ((SCREEN_HEIGHT / 2) * SCREEN_WIDTH) //top right
+		((SCREEN_WIDTH / 2) - 1) + ((SCREEN_HEIGHT / 2) * SCREEN_WIDTH) + SCREEN_WIDTH //bottom left
+		(SCREEN_WIDTH / 2) + ((SCREEN_HEIGHT / 2) * SCREEN_WIDTH) + SCREEN_WIDTH //bottom right
+
+	*/
+
+	//sets all pixels to green
+	for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; i++) {
+		buffer[i] = 0x00FF00FF;
+	}
+
+		//sets the 4 center pixels to red
+		int x = (SCREEN_WIDTH / 2) - 1, y = (SCREEN_HEIGHT / 2) * SCREEN_WIDTH;
+		buffer[x + y] = 0xFF0000FF;
+		buffer[x + y + 1] = 0xFF0000FF;
+		buffer[x + y + SCREEN_WIDTH] = 0xFF0000FF;
+		buffer[x + y + 1 + SCREEN_WIDTH] = 0xFF0000FF;
 
 	SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH * sizeof(Uint32));
 	SDL_RenderClear(renderer);

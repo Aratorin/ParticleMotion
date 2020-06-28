eba42b7834fca0e9668e7e847c6d2d1bc5c6bc97
Aratorin <johnjhofmann3@gmail.com>
2020-06-28 16:33:16 -0400

    Basic SDL Window.
    - Created the SDL_Window object.
    - Created the game loop that runs until an SDL_Quit event is called.

--- ParticleMotion/ParticleMotion.cpp
+++ ParticleMotion/ParticleMotion.cpp
@@ -8,8 +8,33 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
-	cout << "SDL_Init succeeded!" << endl;
+	const int SCREEN_WIDTH = 800;
+	const int SCREEN_HEIGHT = 600;
 
+	SDL_Window* window = SDL_CreateWindow("Particle Motion", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+	if (window == NULL) {
+		SDL_Quit;
+		return 2;
+	}
+
+	bool quit = false;
+
+	SDL_Event event;
+
+	while (!quit) {
+		//Update particles
+		//Draw particles
+		//Check for messages/events
+
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				quit = true;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 
 	return 0;
